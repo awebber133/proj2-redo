9685587e4dda55d73ff9c5f9fa03e6d145a6c583
Alexis Webber <alexis.webber@maritz.com>
2026-04-26 19:08:34 -0500

    Remove accidental node-app folder

--- node-app/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-
-app.get('/', (req, res) => res.send('Hello World! This is the Node.js version!'));
-
-app.listen(port, () => console.log(`App running on port ${port}`));
